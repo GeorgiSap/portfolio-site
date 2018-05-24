0f157c32c96c8879d03848ffbe1a20519e44a56b
g.sapundzhiev <g.sapundzhiev@blubito.com>
2018-05-24 14:40:09 +0300

    Added model

---
+++ model.js
@@ -0,0 +1,53 @@
+const model = {
+    appListings: [{
+            appImageSource: 'images/pixel-art-maker.PNG',
+            appImageAltText: 'Pixel Art Maker',
+            appTitle: 'Pixel Art Maker',
+            appLinks: [{
+                    linkUrl: 'https://github.com/GeorgiSap/pixel-art-maker',
+                    linkTitle: 'Github Repo'
+                },
+                {
+                    linkUrl: 'https://htmlpreview.github.io/?https://github.com/GeorgiSap/pixel-art-makerhttps://github.com/GeorgiSap/pixel-art-maker/blob/master/index.html',
+                    linkTitle: 'Html Preview'
+                }
+            ],
+            appDescription: 'Single-page web app that allows users to draw pixel art on a customizable canvas. Choose a color. Click a cell to fill that cell with the chosen color, fill multiple cells with color, erase or pick a color. Use Undo and Redo to make art easily.'
+        },
+        {
+            appImageSource: 'images/recycler-logo.png',
+            appImageAltText: 'Recycler',
+            appTitle: 'Recycler',
+            appLinks: [{
+                    linkUrl: 'https://play.google.com/store/apps/details?id=com.skatingpirate.razdelko',
+                    linkTitle: 'Google Play'
+                },
+                {
+                    linkUrl: 'https://itunes.apple.com/us/app/recycler-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB%D0%BA%D0%BE/id1083728246?ls=1&mt=8',
+                    linkTitle: 'Apple Store'
+                },
+                {
+                    linkUrl: 'http://bulecopack.com/razdelko/',
+                    linkTitle: 'Browser'
+                }
+            ],
+            appDescription: 'Learn how to sort out waste and how to use the colored containers for separate collection of packaging waste. Contribute to protecting the environment and reducing the amount of waste with the help of Recycler and his friends.'
+        },
+
+        {
+            appImageSource: 'images/skating-pirate-logo.png',
+            appImageAltText: 'Skating Pirate',
+            appTitle: 'Skating Pirate',
+            appLinks: [{
+                    linkUrl: 'https://play.google.com/store/apps/details?id=com.skatingpirate.skatingpirate',
+                    linkTitle: 'Google Play'
+                },
+                {
+                    linkUrl: 'https://itunes.apple.com/us/app/skating-pirate/id1004540361?ls=1&mt=8',
+                    linkTitle: 'Apple Store'
+                }
+            ],
+            appDescription: 'Challenge your reactions with the addictive endless runner \'Skating Pirate \'. TAP at the right moment to avoid obstacles. Go aboard a ship and collect as many coins as possible. Compare scores with others and unlock pirate achievements.'
+        }
+    ]
+}
